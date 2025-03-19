54c5c39d14d47bd5c0c1e7584d9ff2e36ecd273b
Muhammed Shafein hashmi <230010178@aston.ac.uk>
2025-03-19 00:46:17 +0000

    current working

--- resources/views/wishlist2.blade.php
+++ resources/views/wishlist2.blade.php
@@ -14,7 +14,7 @@
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Dela+Gothic+One&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="{{ asset('/css/dark-mode-styles/Wishlist-dark-mode.css') }}">
+  <link rel="stylesheet" href="{{ asset('/css/dark-mode-styles/wishlist-dark-mode.css') }}">
   <link rel="stylesheet" href="{{ asset('/css/wishlist.css') }}">
 </head>
 
